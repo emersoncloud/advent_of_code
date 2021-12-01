07bc2355250c7bdf305549a079d2b891792b5a5c
Emerson Cloud <emerson.cloud@gmail.com>
2021-12-01 15:07:25 -0500

    First draft part two

--- advent_of_code_1/src/main.rs
+++ advent_of_code_1/src/main.rs
@@ -1,13 +1,15 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
- 
+
 fn main() {
-    let file = File::open("./input.txt").unwrap();
-    let reader = BufReader::new(file);
-    // for kyle to show me how vscode works
+    part_one();
+    part_two();
+}
+
+fn part_one() {
+    let reader = open_file("input.txt");
 
     let mut depths: Vec<u32> = Vec::new();
-
     for line in reader.lines() {
         if let Ok(ip) = line {
             depths.push(ip.parse().unwrap());
@@ -16,13 +18,47 @@ fn main() {
 
     let mut depth_increases: u32 = 0;
     let mut prev: u32 = std::u32::MAX;
-
     for depth in depths.iter() {
         if *depth > prev {
             depth_increases += 1;
         }
         prev = *depth;
     }
-    println!("{}", depth_increases);
-    
+
+    println!("depths: {}", depth_increases);
+}
+
+fn part_two() {
+    let reader = open_file("input.txt");
+
+    let mut depths: Vec<u32> = Vec::new();
+    for line in reader.lines() {
+        if let Ok(ip) = line {
+            depths.push(ip.parse().unwrap());
+        }
+    }
+
+    let mut rolling_depths = 0;
+
+    let mut rolling_one: u32 = depths[2] + depths[1] + depths[0];
+    let mut rolling_two: u32;
+    for i in 2..depths.len() {
+        rolling_two = depths[i] + depths[i-1] + depths[i-2];
+        if rolling_two > rolling_one {
+            rolling_depths +=1;
+        }
+        rolling_one = rolling_two;
+    }
+
+    println!("rolling depths: {}", rolling_depths);
+
+}
+
+fn open_file(filename: &str) -> BufReader<File> {
+    let file = match File::open(filename) {
+        Ok(f) => f,
+        Err(e) => panic!("unable to open file {:?}", e),
+    };
+
+    BufReader::new(file)
 }
