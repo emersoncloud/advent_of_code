e82fcb46257705e0fcd56a31a08d5b200e33b1f5
Emerson Cloud <emerson.cloud@gmail.com>
2021-12-01 10:32:52 -0500

    part one rough draft

--- advent_of_code_1/src/main.rs
+++ advent_of_code_1/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+ 
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut depths: Vec<u32> = Vec::new();
+
+    for line in reader.lines() {
+        if let Ok(ip) = line {
+            depths.push(ip.parse().unwrap());
+        }
+    }
+
+    let mut depth_increases: u32 = 0;
+    let mut prev: u32 = std::u32::MAX;
+
+    for depth in depths.iter() {
+        if *depth > prev {
+            depth_increases += 1;
+        }
+        prev = *depth;
+    }
+    println!("{}", depth_increases);
+    
 }
