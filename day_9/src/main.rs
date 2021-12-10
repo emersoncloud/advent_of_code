b178a8b2701938a083deba23c1f6b3847d9d760d
Emerson Cloud <emerson.cloud@gmail.com>
2021-12-10 00:05:48 -0500

    clean it up

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -5,6 +5,7 @@ use std::time::Instant;
 const GRID_SIZE: usize = 100;
 
 fn main() {
+    let mut time = Instant::now();
     let reader = BufReader::new(File::open("input.txt").expect("unable to open file"));
     let mut grid: [[i32; GRID_SIZE]; GRID_SIZE] = [[0; GRID_SIZE]; GRID_SIZE];
     for (idx, line) in reader.lines().flatten().enumerate() {
@@ -17,35 +18,34 @@ fn main() {
             grid[idx][jdx] = x;
         }
     }
+    println!("Time to create initial state: {:?}", time.elapsed());
+
+    time = Instant::now();
+    let lin_ans = linear(&grid).iter().fold(0, |acc, val| acc + val + 1);
+    println!("linear danger: {}", lin_ans);
+    println!("linear duration: {:?}", time.elapsed());
 
     let mut local_mins_dfs: Vec<i32> = Vec::new();
     let mut visited: [[bool; GRID_SIZE]; GRID_SIZE] = [[false; GRID_SIZE]; GRID_SIZE];
-
-    let mut start = Instant::now();
-    let local_mins_linear: Vec<i32> = linear(&grid);
-    let linear_duration = start.elapsed();
-
-    start = Instant::now();
+    time = Instant::now();
     dfs(0, 0, &grid, &mut local_mins_dfs, &mut visited);
-    let dfs_duration = start.elapsed();
-
-    let lin_ans = local_mins_linear.iter().fold(0, |acc, val| acc + val + 1);
-    println!("linear danger: {}", lin_ans);
-    println!("linear duration: {:?}", linear_duration);
-
+    let dfs_duration = time.elapsed();
     let dfs_ans = local_mins_dfs.iter().fold(0, |acc, val| acc + val + 1);
     println!("dfs danger: {}", dfs_ans);
     println!("dfs duration: {:?}", dfs_duration);
 
-    start = Instant::now();
+    time = Instant::now();
     visited = [[false; GRID_SIZE]; GRID_SIZE];
     let basin_count = count_basins(&grid, &mut visited);
-    let basin_duration = start.elapsed();
+    let basin_duration = time.elapsed();
     println!("count basins: {}", basin_count);
     println!("basin duration: {:?}", basin_duration);
 }
 
-fn count_basins(grid: &[[i32; GRID_SIZE]; GRID_SIZE], visited: &mut [[bool; GRID_SIZE]; GRID_SIZE]) -> i32 {
+fn count_basins(
+    grid: &[[i32; GRID_SIZE]; GRID_SIZE],
+    visited: &mut [[bool; GRID_SIZE]; GRID_SIZE],
+) -> i32 {
     let mut basins: Vec<i32> = Vec::new();
     for i in 0..grid.len() {
         for j in 0..grid[i].len() {
@@ -65,7 +65,12 @@ fn count_basins(grid: &[[i32; GRID_SIZE]; GRID_SIZE], visited: &mut [[bool; GRID
     sum
 }
 
-fn basin_dfs(i: i32, j: i32, grid: &[[i32; GRID_SIZE]; GRID_SIZE], visited: &mut [[bool; GRID_SIZE]; GRID_SIZE]) -> i32 {
+fn basin_dfs(
+    i: i32,
+    j: i32,
+    grid: &[[i32; GRID_SIZE]; GRID_SIZE],
+    visited: &mut [[bool; GRID_SIZE]; GRID_SIZE],
+) -> i32 {
     if !is_valid_coords(i, j, visited) {
         return 0;
     }
@@ -73,12 +78,10 @@ fn basin_dfs(i: i32, j: i32, grid: &[[i32; GRID_SIZE]; GRID_SIZE], visited: &mut
     if grid[i as usize][j as usize] == 9 {
         return 0;
     }
-    return 1 +
-        basin_dfs(i+1, j, grid, visited) +
-        basin_dfs(i-1, j, grid, visited) +
-        basin_dfs(i, j-1, grid, visited) +
-        basin_dfs(i, j+1, grid, visited);
-
+    1 + basin_dfs(i + 1, j, grid, visited)
+        + basin_dfs(i - 1, j, grid, visited)
+        + basin_dfs(i, j - 1, grid, visited)
+        + basin_dfs(i, j + 1, grid, visited)
 }
 
 fn linear(grid: &[[i32; GRID_SIZE]; GRID_SIZE]) -> Vec<i32> {
@@ -100,22 +103,18 @@ fn dfs(
     local_mins: &mut Vec<i32>,
     visited: &mut [[bool; GRID_SIZE]; GRID_SIZE],
 ) {
+    if !is_valid_coords(i, j, visited) {
+        return;
+    }
     visited[i as usize][j as usize] = true;
     if is_local_min(i as usize, j as usize, grid) {
         local_mins.push(grid[i as usize][j as usize]);
     }
-    if is_valid_coords(i - 1, j, visited) {
-        dfs(i - 1, j, grid, local_mins, visited);
-    }
-    if is_valid_coords(i + 1, j, visited) {
-        dfs(i + 1, j, grid, local_mins, visited);
-    }
-    if is_valid_coords(i, j + 1, visited) {
-        dfs(i, j + 1, grid, local_mins, visited);
-    }
-    if is_valid_coords(i, j - 1, visited) {
-        dfs(i, j - 1, grid, local_mins, visited);
-    }
+
+    dfs(i - 1, j, grid, local_mins, visited);
+    dfs(i + 1, j, grid, local_mins, visited);
+    dfs(i, j + 1, grid, local_mins, visited);
+    dfs(i, j - 1, grid, local_mins, visited);
 }
 
 fn is_local_min(i: usize, j: usize, grid: &[[i32; GRID_SIZE]; GRID_SIZE]) -> bool {
