635c4bb9f9a5798367cc79a55ae78d9de8be75ee
Emerson Cloud <emerson.cloud@gmail.com>
2021-12-14 23:47:12 -0500

    part one and two

---
+++ day_14/src/main.rs
@@ -0,0 +1,158 @@
+use std::io::{prelude::*, BufReader};
+use std::fs::File;
+use std::time::Instant;
+use std::collections::HashMap;
+
+fn main() {
+    let mut reader = BufReader::new(File::open("test_input.txt").unwrap());
+
+    let mut input_string: String = String::new();
+    let mut bogus: String = String::new();
+    reader.read_line(&mut input_string).expect("work please!");
+    reader.read_line(&mut bogus).expect("please work");
+
+    input_string.pop();
+
+    let mut map: HashMap<String, String> = HashMap::new();
+    for line in reader.lines().flatten() {
+        let input = line.split("->")
+            .collect::<Vec<&str>>();
+
+        map.insert(
+            String::from(input.get(0).unwrap().strip_suffix(' ').unwrap()),
+            String::from(input.get(1).unwrap().strip_prefix(' ').unwrap())
+        );
+    }
+
+    part_one(&mut input_string, map.clone(), 10);
+    part_two(&mut input_string, &map.clone(), 40);
+}
+
+fn part_two(pass_in_string: &String, map: &HashMap<String, String>, iters: i32) {
+    let mut input_string: String = pass_in_string.clone();
+    let timer = Instant::now();
+    let mut output: HashMap<String, i64> = HashMap::new();
+    let mut input: HashMap<String, i64> = HashMap::new();
+
+    for i in 0..(input_string.len() - 1) {
+        let pair = &input_string[i..i+2];
+        let current_val = input.get(pair);
+        if current_val.is_some() {
+            input.insert(String::from(pair), current_val.unwrap() + 1);
+        } else {
+            input.insert(String::from(pair), 1);
+        }
+    }
+
+    for i in 0..iters {
+        output.clear();
+        input.iter().for_each(|pair| {
+            let new_strigns = get_strings(pair.0,  map);
+            for new in new_strigns {
+                let current_val = output.get(new.as_str());
+                if current_val.is_some() {
+                    output.insert(new, current_val.unwrap() + *pair.1);
+                } else {
+                    output.insert(new, *pair.1);
+                }
+            }
+
+
+        });
+        input = output.clone();
+    }
+
+
+    let mut count_chars: HashMap<char, i64> = HashMap::new();
+    output.iter().for_each(|o| {
+        for c in o.0.as_bytes() {
+            let charmander = *c as char;
+            let count = count_chars.get(&(*c as char));
+            if count.is_none() {
+                count_chars.insert(*c as char, *o.1);
+            } else {
+                count_chars.insert(*c as char, count.unwrap() + *o.1);
+            }
+        }
+    });
+
+    let start_char = *input_string.as_bytes().get(0).unwrap() as char;
+    let vec = input_string.as_bytes();
+    let end_char = *input_string.as_bytes().get(vec.len()-1).unwrap() as char;
+    let mut real_const_chars: HashMap<char, i64> = HashMap::new();
+    for c in count_chars.iter() {
+        if *c.0 == start_char || *c.0 == end_char {
+            real_const_chars.insert(*c.0, c.1/2 + 1);
+        } else {
+            real_const_chars.insert(*c.0, c.1/2);
+        }
+    }
+
+    println!("timer: {:?}", timer.elapsed());
+    print_output(real_const_chars);
+}
+
+fn print_output(map: HashMap<char, i64>) {
+    let mut biggest: i64 = i64::MIN;
+    let mut smallest: i64 = i64::MAX;
+    for (_, value) in map.iter() {
+        if *value > biggest {
+            biggest = *value;
+        }
+        if *value < smallest {
+            smallest = *value;
+        }
+    }
+    println!("{:?}", map);
+    println!("difference: big, small, diff {}, {}, {}", biggest, smallest, biggest - smallest);
+
+}
+
+fn get_strings(pair: &String, map: &HashMap<String, String>) -> Vec<String> {
+    let mut string_one = String::new();
+
+    string_one.push_str(pair[0..1].to_string().as_str());
+    string_one.push_str(map.get(pair).unwrap());
+
+    let mut string_two = String::new();
+    string_two.push_str(map.get(pair).unwrap());
+    string_two.push_str(pair[1..2].to_string().as_str());
+    return vec![string_one, string_two];
+}
+
+fn part_one(pass_in_string: &String, map: HashMap<String, String>, iters: i32) {
+    let mut input_string = pass_in_string.clone();
+    let timer = Instant::now();
+    let mut output_string: String = String::new();
+    for i in 0..iters {
+        output_string.clear();
+        for i in 0..(input_string.len() - 1) {
+            let input = &input_string[i..i+2];
+            if i == 0 {
+                output_string.push_str(&input_string[i..i+1]);
+            }
+            if let val = map.get(&input_string[i..i+2]).unwrap() {
+                output_string.push_str(val);
+            }
+
+            output_string.push_str(&input_string[i+1..i+2]);
+        }
+        println!("iter: {}, string_len: {}", i, output_string.len());
+        input_string = output_string.clone();
+    }
+
+    let mut count_chars: HashMap<char, i64> = HashMap::new();
+    output_string.as_bytes().iter().for_each( |c| {
+
+        let count = count_chars.get(&(*c as char));
+        if count.is_none() {
+            count_chars.insert(*c as char, 1);
+        } else {
+            count_chars.insert(*c as char, count.unwrap() + 1);
+        }
+    });
+
+    println!("timer: {:?}", timer.elapsed());
+    print_output(count_chars);
+}
+
